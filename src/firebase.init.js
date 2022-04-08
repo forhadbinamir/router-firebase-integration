0cacc1894015e82fec2b67fcd4eda189360aa3c6
forhadbinamir <forhadbinamir@gmail.com>
2022-04-09 02:11:24 +0300

    router-firebase-integration-website

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyByh69V54LZUw7pA_TrzsKxEtUPyvD5y4c",
+    authDomain: "router-firebase-integrat-7c03e.firebaseapp.com",
+    projectId: "router-firebase-integrat-7c03e",
+    storageBucket: "router-firebase-integrat-7c03e.appspot.com",
+    messagingSenderId: "225883604464",
+    appId: "1:225883604464:web:4fbac359b12ac24a3c4935"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
